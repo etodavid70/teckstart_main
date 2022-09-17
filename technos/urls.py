d410b8016669c688e7075dc0bf5e51980d1d62b1
Eto David <103228048+etodavid70@users.noreply.github.com>
2022-09-17 10:54:20 +0100

    Add files via upload

---
+++ technos/urls.py
@@ -0,0 +1,25 @@
+"""technos URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from teck import views
+
+urlpatterns = [
+    path('', views.index_call),
+    path('mobile', views.mobile_app_call),
+    path('error500', views.error500_call),
+    path('admin/', admin.site.urls),
+]
